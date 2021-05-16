81422c2dceecff6fd818997f86a53a169573b005
Mobidikt <ghost3299@yandex.ru>
2021-05-16 12:28:32 +0400

    Add coordinates of cities and block forecast date

--- src/components/ForecastDay/ForecastDay.tsx
+++ src/components/ForecastDay/ForecastDay.tsx
@@ -4,6 +4,12 @@ import './ForecastDay.css';
 const ForecastDay: React.FC = () => {
   return (
     <div className="forecast-day">
+        <h2 className="forecast-day__title">Forecast for a Date in the Past</h2>
+        <div className="forecast-day__menu"></div>
+        <div className="forecast-day__weather">
+            <p className="forecast-day__date">Date</p>
+            <p className="forecast-day__degree">degrees</p>
+        </div>
     </div>
   );
 };
---
+++ src/config.ts
@@ -0,0 +1,27 @@
+export const list_cities: {name: string, longitude: number, latitude: number}[] = [
+    {
+        name: 'Самара',
+        longitude: 53.195873,
+        latitude: 50.100193,
+    },
+    {
+        name: 'Тольятти',
+        longitude: 53.507836,
+        latitude: 49.420393,
+    },
+    {
+        name: 'Саратов',
+        longitude: 51.533557,
+        latitude: 46.034257,
+    },
+    {
+        name: 'Казань',
+        longitude: 55.796127,
+        latitude: 49.106405,
+    },
+    {
+        name: 'Краснодар',
+        longitude: 45.035470,
+        latitude: 38.975313,
+    },
+]
